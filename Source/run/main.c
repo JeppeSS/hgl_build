344ddf29e4558f935a31ee1e329c69ab74e618ec
jeppeSS <jsskov92@gmail.com>
2020-10-11 12:55:59 +0200

    Initial commandline argument parsing

--- Source/run/main.c
+++ Source/run/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int
-main( void )
+main( int argc, char* argv[ argc + 1 ] )
 {
-  fprintf( stdout, "Hello, World!\n" );
+  if( argc > 1 )
+  {
+    if( strcmp( argv[ 1 ], "new" ) == 0 )
+    {
+      if( argc == 3 )
+      {
+        fprintf( stdout, "Creating new project: %s\n", argv[ 2 ] );
+      }
+      else
+      {
+        fprintf( stdout, "Missing argument for project name.\n" );
+      }
+    }
+    else
+    {
+      fprintf( stdout, "Invalid command: %s\n", argv[ 1 ] );
+    }
+  }
+
   return EXIT_SUCCESS;
 }
--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc -std=c18 -Wall -Wextra -Werror -pedantic-errors Source/run/main.c
+gcc -std=c18 -Wall -Wextra -Werror -pedantic-errors Source/run/main.c -o Hagall
