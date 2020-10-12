4c0f48e4829f4f548b7aedbbf0b6737b6f201d57
jeppeSS <jsskov92@gmail.com>
2020-10-12 21:48:29 +0200

    Building folders + minor refac

--- Source/run/main.c
+++ Source/run/main.c
@@ -10,6 +10,9 @@
 bool
 is_directory( const char *directory );
 
+void
+new_project( const char *p_root );
+
 int
 main( int argc, char* argv[ argc + 1 ] )
 {
@@ -19,22 +22,7 @@ main( int argc, char* argv[ argc + 1 ] )
     {
       if( argc == 3 )
       {
-        if( !is_directory( argv[ 2 ] ) )
-        {
-	  if( mkdir( argv[ 2 ] ) == 0 )
-          {
-            fprintf( stdout, "Creating new project: %s\n", argv[ 2 ] );
-          }
-          else
-          {
-            fprintf( stdout, "Could not create directory.\n" );
-          }
-        }
-        else
-        {
-          fprintf( stdout, "Folder already exists.\n" );
-        }
-        
+        new_project( argv[ 2 ] ); 
       }
       else
       {
@@ -52,6 +40,53 @@ main( int argc, char* argv[ argc + 1 ] )
   }
 
   return EXIT_SUCCESS;
+}
+
+
+
+void
+new_project( const char *p_root )
+{
+  if( p_root )
+  {
+    if( !is_directory( p_root ) )
+    {
+      if( mkdir( p_root ) == 0 )
+      {
+        fprintf( stdout, "Creating new project: %s...\n", p_root );
+
+        const size_t root_length = strlen( p_root );
+        const size_t path_length = root_length + strlen( "/Source" );
+        const size_t total_length = root_length + path_length;
+
+        char *p_path = strncpy( malloc( sizeof( char[ total_length ] ) ),
+                                p_root, total_length - 1 );
+            
+        p_path = strncat( p_path, "/Source", path_length );
+        p_path[ total_length - 1 ] = '\0';
+            
+        if( mkdir( p_path ) == 0 )
+        {
+        }
+        else
+        {
+          fprintf( stdout, "Could not create directory.\n" );
+        }                     
+      }
+      else
+      {
+        fprintf( stdout, "Could not create directory.\n" );
+      }
+    }
+    else
+    {
+      fprintf( stdout, "Folder already exists.\n" );
+    }
+  }
+  else
+  {
+    fprintf( stdout, "Folder already exists.\n" );
+  } 
 }
 
 bool
