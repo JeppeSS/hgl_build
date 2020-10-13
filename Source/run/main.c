ffe9ccaba7cdf8aee46f6fa9a62e8382c86a4c42
jeppeSS <jsskov92@gmail.com>
2020-10-13 20:40:54 +0200

    Cleanup

--- Source/run/main.c
+++ Source/run/main.c
@@ -14,6 +14,12 @@ is_directory( const char *directory );
 void
 new_project( char *p_root );
 
+void
+create_new_project( char *p_root );
+
+void
+create_initial_folder_structure( char *p_root );
+
 int
 main( int argc, char* argv[ argc + 1 ] )
 {
@@ -23,7 +29,7 @@ main( int argc, char* argv[ argc + 1 ] )
     {
       if( argc == 3 )
       {
-        new_project( argv[ 2 ] ); 
+        create_new_project( argv[ 2 ] ); 
       }
       else
       {
@@ -44,44 +50,40 @@ main( int argc, char* argv[ argc + 1 ] )
 }
 
 
+void
+create_new_project( char *p_root )
+{
+  create_initial_folder_structure( p_root );
+}
 
 void
-new_project( char *p_root )
+create_initial_folder_structure( char *p_root )
 {
   if( p_root )
   {
     if( !is_directory( p_root ) )
     {
-      if( mkdir( p_root ) == 0 )
+      char *p_source_path = hgl_string_concat( p_root, "/Source" );
+      char *p_run_path    = hgl_string_concat( p_source_path, "/run" );
+       
+      if( mkdir( p_root )        == 0 &&
+          mkdir( p_source_path ) == 0 &&
+          mkdir( p_run_path )    == 0 )
       {
-        fprintf( stdout, "Creating new project: %s...\n", p_root );
-	
-        char *p_path = hgl_string_concat( p_root, "/Source" );            
-        if( mkdir( p_path ) == 0 )
-        {
-        }
-        else
-        {
-          fprintf( stdout, "Could not create directory.\n" );
-        }                     
-
-        hgl_string_destroy( p_path );
+        fprintf( stdout, "Folder structure complete.\n" );
       }
       else
       {
-        fprintf( stdout, "Could not create directory.\n" );
+        // TODO[Jeppe]: Add cleanup procedure.
+        fprintf( stdout, "Something went wrong, could not create folders.\n" );
       }
-    }
-    else
-    {
-      fprintf( stdout, "Folder already exists.\n" );
+      
+      hgl_string_destroy( p_source_path );
+      hgl_string_destroy( p_run_path );
     }
   }
-  else
-  {
-    fprintf( stdout, "Folder already exists.\n" );
-  } 
 }
+
 
 bool
 is_directory( const char *directory )
